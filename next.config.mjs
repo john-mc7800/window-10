ddb5149abe0e968598457524c64f6dbecc0ceac5
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-11-01 16:43:35 +0500

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "img.icons8.com",
-      // "https://www.google.com"
-    ],
+      remotePatterns: [
+          {
+              protocol: 'https', 
+              hostname: 'img.icons8.com', 
+              port: '', 
+              pathname: '/**', 
+          },
+      ],
   },
 };
 
